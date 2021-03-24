7cb3a01022f022be77d2f5efeddf84edf1033787
Kozikhodzha <akozikhodzha@gmail.com>
2021-03-24 23:08:07 +0700

    добавлены типы

---
+++ pkg/bank/types/types.go
@@ -0,0 +1,35 @@
+package types
+
+//Money представляет собой денежную сумму в минимальных единицах(центы, копейки, дирамы и т.д.)
+type Money int64
+
+//Cyrrency представляет код валюты
+type Currency string
+
+//Коды валют
+const (
+	TJS Currency = "TJS"
+	RUB Currency = "RUB"
+	USD Currency = "USD"
+)
+
+//Payment
+type Payment struct {
+	ID     int
+	Amount Money
+}
+
+//PAN представляет номер карты
+type PAN string
+
+//Card представляет информацию о платежной карте
+type Card struct {
+	ID         int
+	PAN        PAN
+	Balance    Money //использовали Money
+	MinBalance Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+}
